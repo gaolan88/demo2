28a6ee01792ce5b60d2b0c803a13398d90bdaee9
gl <150550340@qq.com>
2021-05-12 11:02:34 +0800

    5149978972

--- src/main/java/com/wfd360/controller/HelloController.java
+++ src/main/java/com/wfd360/controller/HelloController.java
@@ -12,7 +12,7 @@ public class HelloController {
     @RequestMapping("/data")
     @ResponseBody
     public String data() {
-        System.out.println("---------88345345345534588---------");
-        return "hello world 8345345345345888888";
+        System.out.println("---------656565656--------");
+        return "hello world 8787987887";
     }
 }
